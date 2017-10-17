6f23471e46305cd919eabbef19ba701eb24db0c3
alexNeto <alex.neto3@hotmail.com>
2017-10-17 17:57:19 -0200

    feito function.php

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+
+try {
+    if (! @include_once( './secret.php' )) {
+        throw new Exception ('secret.php does not exist');
+    }
+    if (!file_exists('./secret.php' )) {
+        throw new Exception ('secret.php does not exist');
+    } else {
+        require_once('./secret.php' ); 
+    }      
+} catch(Exception $e) { 
+    echo "Message : " . $e->getMessage();
+    echo "Code : " . $e->getCode();
+}
+
+require_once 'secret.php';
+
+$appName = 'gesichtsbuch';
+
+$conection = new mysqli($hn, $un, $pw, $db);
+
+if($conection->connect_error) {
+    die($conection->connect_error);
+}
+
+function createTable($name, $query) {
+    queryMysql("CREATE TABLE IF NOT EXITS $name($query)");
+    echo "Table '$name' created or already exists.<br>"; 
+}
+
+function queryMysql($query) {
+    global $conection;
+    $result = $conection->query($query);
+    if($result) {
+        die($conection->error);
+        return $result;
+    }
+}
+
+function destroySession() {
+    $_SESSION = array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time() - 2592000, '/');
+    }
+
+    session_destroy();
+}
+
+function sanitizeString($var) {
+    global $conection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $conection->real_escape_string($var);
+}
+
+function showProfile($user) {
+    if (file_exists("$user.jpg")) {
+        echo "<img src='$user.jpg' style='float: left;' ";
+    }
+
+    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows) {
+        $row = $result->fetch_array(MYSQL_ASSOC);
+        echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+}
+
+?>
---
+++ secret-example.php
@@ -0,0 +1,8 @@
+<?php
+
+$hn = '127.0.0.1'; // The host address
+$db = 'database';  // The database name
+$un = 'user'; // The user name for the database
+$pw = 'password'; // The password for the database
+
+?>
